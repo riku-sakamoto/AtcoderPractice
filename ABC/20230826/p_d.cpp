76f2ae9aa1b11636a33ae90c7c4415b39bcd451e
Riku Sakamoto <rikusaka03@gmail.com>
2023-08-28 14:53:52 +0900

    add virtual contests

---
+++ ABC/20230826/p_d.cpp
@@ -0,0 +1,184 @@
+#ifdef __LOCAL
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+#include <vector>
+#include <queue>
+#include <set>
+#include <iostream>
+#include <iomanip>
+using namespace std;
+typedef long long LL;
+#define REP(i,n) for(int i=0;i<(n);++i)
+#define REPREV(i,n) for(int i=(n-1);i>=(0);--i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define ALL(x) x.begin(),x.end()
+#define BIT(x,i)(((x)>>(i))&1)
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
+void Yes() {cout << "Yes" << endl;}
+void No() {cout << "No" << endl;}
+void YES() {cout << "YES" << endl;}
+void NO() {cout << "NO" << endl;}
+template <class T> inline void out(T a){cout << a << endl;}
+template<typename T> int larger_equal(vector<T>& vec, T val){
+    auto ptr = lower_bound(ALL(vec), val);
+    int idx = ptr - vec.begin();
+    return idx;
+}
+template<typename T> int larger_than(vector<T>& vec, T val){
+    auto ptr = upper_bound(ALL(vec), val);
+    int idx = ptr - vec.begin();
+    return idx;
+}
+template<typename T> int lower_equal(vector<T>& vec, T val){
+    auto ptr = upper_bound(ALL(vec), val);
+    int idx = ptr - vec.begin() - 1;
+    return idx;
+}
+template<typename T> int lower_than(vector<T>& vec, T val){
+    auto ptr = lower_bound(ALL(vec), val);
+    int idx = ptr - vec.begin() - 1;
+    return idx;
+}
+template<typename T> inline void out_vector(vector<T>& vec){
+    REP(i, vec.size()){cout << vec[i] << " ";}
+    cout << endl;
+}
+template <typename T> struct Matrix {
+    vector<vector<T>> _array;
+    int _Nr;
+    int _Nc;
+    Matrix(int Nr, int Nc): _Nr(Nr), _Nc(Nc) {
+        _array = vector(Nr, vector<T>(Nc));
+    };
+
+    void to_eye(){
+        REP(i, _Nr){
+            REP(j, _Nc){
+                if(i == j){
+                    _array[i][j] = T(1);
+                }else{
+                    _array[i][j] = T(0);
+                }
+            }
+        }
+    }
+
+    vector<T>& operator[] (int i){
+        return _array[i];
+    }
+
+    Matrix operator* (Matrix& other){
+        if(_Nc != other._Nr){
+            throw runtime_error("Cannot multiply two matrix");
+        }
+
+        Matrix<T> res(_Nr, other._Nc);
+        REP(i, _Nr){
+            REP(j, other._Nc){
+                T val = T(0);
+                REP(k, _Nc){
+                    val += _array[i][k] * other._array[k][j];
+                }
+                res[i][j] = val;
+            }
+        }
+        return res;
+    }
+
+    void show(){
+        REP(i, _Nr){
+            REP(j, _Nc){
+                cout << _array[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+template <typename T> T calcPow(T& val, LL n, T& init_val){
+    T ans = init_val;
+    while(n > 0){
+        if((n & 1) == 1){
+            ans = val * ans;
+        }
+        val = val * val;
+        n = (n >> 1);
+    }
+    return ans;
+};
+struct Edge {
+    int to;
+    LL w;
+    Edge(int to, LL w): to(to), w(w) {}
+};
+string str_slice(int i, int j, string& S){
+    int n = j - i;
+    return S.substr(i, n);
+}
+using Graph = vector<vector<Edge>>;
+const LL INF = 1LL << 60;
+const int inf = INT_MAX / 2;
+
+
+int main(){
+    int N; cin >> N;
+    vector<tuple<LL, LL, int>> elections(N);
+    int sum_z = 0;
+    REP(i, N){
+        LL x, y; int z; cin >> x >> y >> z;
+        elections[i] = {x, y, z};
+        sum_z += z;
+    }
+
+    int t_val = 0;
+    int a_val = 0;
+    vector<pair<LL, int>> cost_gain;
+    REP(i, N){
+        LL x, y; int z;
+        tie(x, y, z) = elections[i];
+        if(y > x){a_val += z;}
+        else{t_val += z;}
+
+        if(y > x){
+            LL cost = (y - x + 1LL) / 2LL;
+            cost_gain.push_back({cost, z});
+        }
+    }
+
+    if(t_val > a_val){
+        out(0); return 0;
+    }
+
+    int n = cost_gain.size();
+    vector<vector<LL>> dp(n + 1, vector<LL>(sum_z + 1, INF));
+    REP(i, n + 1){dp[i][0] = 0LL;}
+    // vector<LL> dp(sum_z + 1, INF);
+    // dp[0] = 0LL;
+
+    REP(i, n){
+        auto p = cost_gain[i];
+        LL cost = p.first;
+        int gain = p.second;
+        REP(j, sum_z + 1){
+            auto min_v = max({j - gain, 0});
+            if(dp[i][min_v] == INF){continue;}
+
+            auto new_cost = cost + dp[i][min_v];
+            auto new_v = min({dp[i][j], new_cost});
+            chmin(dp[i + 1][j], new_v);
+        }
+    }
+
+    auto n_cost = (a_val - t_val + 1LL) / 2LL;
+    LL ans = INF;
+    FOR(j, n_cost, sum_z + 1){
+        chmin(ans, dp[n][j]);
+    }
+
+    out(ans);
+
+
+    return 0;
+}
---
+++ ABC/20230826/p_e.cpp
@@ -0,0 +1,217 @@
+#ifdef __LOCAL
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+#include <vector>
+#include <queue>
+#include <set>
+#include <iostream>
+#include <iomanip>
+using namespace std;
+typedef long long LL;
+#define REP(i,n) for(int i=0;i<(n);++i)
+#define REPREV(i,n) for(int i=(n-1);i>=(0);--i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define ALL(x) x.begin(),x.end()
+#define BIT(x,i)(((x)>>(i))&1)
+template<typename T1,typename T2> inline void chmin(T1 &a,T2 b){if(a>b) a=b;}
+template<typename T1,typename T2> inline void chmax(T1 &a,T2 b){if(a<b) a=b;}
+void Yes() {cout << "Yes" << endl;}
+void No() {cout << "No" << endl;}
+void YES() {cout << "YES" << endl;}
+void NO() {cout << "NO" << endl;}
+template <class T> inline void out(T a){cout << a << endl;}
+template<typename T> int larger_equal(vector<T>& vec, T val){
+    auto ptr = lower_bound(ALL(vec), val);
+    int idx = ptr - vec.begin();
+    return idx;
+}
+template<typename T> int larger_than(vector<T>& vec, T val){
+    auto ptr = upper_bound(ALL(vec), val);
+    int idx = ptr - vec.begin();
+    return idx;
+}
+template<typename T> int lower_equal(vector<T>& vec, T val){
+    auto ptr = upper_bound(ALL(vec), val);
+    int idx = ptr - vec.begin() - 1;
+    return idx;
+}
+template<typename T> int lower_than(vector<T>& vec, T val){
+    auto ptr = lower_bound(ALL(vec), val);
+    int idx = ptr - vec.begin() - 1;
+    return idx;
+}
+template<typename T> inline void out_vector(vector<T>& vec){
+    REP(i, vec.size()){cout << vec[i] << " ";}
+    cout << endl;
+}
+template <typename T> struct Matrix {
+    vector<vector<T>> _array;
+    int _Nr;
+    int _Nc;
+    Matrix(int Nr, int Nc): _Nr(Nr), _Nc(Nc) {
+        _array = vector(Nr, vector<T>(Nc));
+    };
+
+    void to_eye(){
+        REP(i, _Nr){
+            REP(j, _Nc){
+                if(i == j){
+                    _array[i][j] = T(1);
+                }else{
+                    _array[i][j] = T(0);
+                }
+            }
+        }
+    }
+
+    vector<T>& operator[] (int i){
+        return _array[i];
+    }
+
+    Matrix operator* (Matrix& other){
+        if(_Nc != other._Nr){
+            throw runtime_error("Cannot multiply two matrix");
+        }
+
+        Matrix<T> res(_Nr, other._Nc);
+        REP(i, _Nr){
+            REP(j, other._Nc){
+                T val = T(0);
+                REP(k, _Nc){
+                    val += _array[i][k] * other._array[k][j];
+                }
+                res[i][j] = val;
+            }
+        }
+        return res;
+    }
+
+    void show(){
+        REP(i, _Nr){
+            REP(j, _Nc){
+                cout << _array[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+template <typename T> T calcPow(T& val, LL n, T& init_val){
+    T ans = init_val;
+    while(n > 0){
+        if((n & 1) == 1){
+            ans = val * ans;
+        }
+        val = val * val;
+        n = (n >> 1);
+    }
+    return ans;
+};
+struct Edge {
+    int to;
+    LL w;
+    Edge(int to, LL w): to(to), w(w) {}
+};
+string str_slice(int i, int j, string& S){
+    int n = j - i;
+    return S.substr(i, n);
+}
+using Graph = vector<vector<Edge>>;
+const LL INF = 1LL << 60;
+const int inf = INT_MAX / 2;
+
+
+int main(){
+    int H, W; cin >> H >> W;
+    vector<vector<char>> A(H, vector<char>(W));
+    pair<int, int> start, goal;
+    REP(i, H){
+        REP(j, W){
+            cin >> A[i][j];
+            if(A[i][j] == 'S'){start = {i, j};}
+            if(A[i][j] == 'G'){goal = {i, j};}
+        }
+    }
+
+    REP(i, H){
+        bool seen = false;
+        REP(j, W){
+            if(A[i][j] == '>'){seen = true; continue;}
+            if(seen){
+                if(A[i][j] == '.' || A[i][j] == '!'){
+                    A[i][j] = '!'; continue;
+                }
+            }
+
+            if(A[i][j] != '.'){seen = false; continue;}
+        }
+        seen = false;
+        REPREV(j, W){
+            if(A[i][j] == '<'){seen = true; continue;}
+            if(seen){
+                if(A[i][j] == '.' || A[i][j] == '!'){
+                    A[i][j] = '!'; continue;
+                }
+            }
+
+            if(A[i][j] != '.'){seen = false; continue;}
+        }
+    }
+
+    REP(j, W){
+        bool seen = false;
+        REP(i, H){
+            if(A[i][j] == 'v'){seen = true; continue;}
+            if(seen){
+                if(A[i][j] == '.' || A[i][j] == '!'){
+                    A[i][j] = '!'; continue;
+                }
+            }
+            if(A[i][j] != '.'){seen = false; continue;}
+        }
+        seen = false;
+        REPREV(i, H){
+            if(A[i][j] == '^'){seen = true; continue;}
+            if(seen){
+                if(A[i][j] == '.' || A[i][j] == '!'){
+                    A[i][j] = '!'; continue;
+                }
+            }
+
+            if(A[i][j] != '.'){seen = false; continue;}
+        }
+    }
+
+    queue<tuple<int, int, int>> que;
+    vector<vector<bool>> seen(H, vector<bool>(W, false));
+    vector<pair<int, int>> dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    que.push({start.first, start.second, 0});
+    seen[start.first][start.second] = true;
+    while(!que.empty()){
+        int i, j, c;
+        tie(i, j, c) = que.front(); que.pop();
+        // cout << i << ", " << j << endl;
+
+        if(i == goal.first && j == goal.second){
+            out(c); return 0;
+        }
+        for(auto d: dirs){
+            int next_i = i + d.first;
+            int next_j = j + d.second;
+            if(next_i < 0 || next_i >= H || next_j < 0 || next_j >= W){continue;}
+            if(seen[next_i][next_j]){continue;}
+            if(A[next_i][next_j] == '.' || A[next_i][next_j] == 'G'){
+                que.push({next_i, next_j, c + 1});
+                seen[next_i][next_j] = true;
+            }
+        }
+    }
+    out(-1);
+
+    // REP(i, H){
+    //     out_vector(A[i]);
+    // }
+
+    return 0;
+}
